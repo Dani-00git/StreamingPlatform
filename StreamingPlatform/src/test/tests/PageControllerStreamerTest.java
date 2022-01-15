15118c94957c79f8fcf8191d411f49874b5deead
Lorenzo <Lorenzo@LAPTOP-ROIL4PLI>
2022-01-15 11:30:51 +0100

    fgfgf

---
+++ StreamingPlatform/src/test/tests/PageControllerStreamerTest.java
@@ -0,0 +1,66 @@
+package tests;
+
+	import static org.junit.jupiter.api.Assertions.*;
+	import org.junit.jupiter.api.Test;
+	import businessLogic.PageControllerStreamer;
+	import static org.junit.Assert.assertEquals; 
+
+	import org.junit.jupiter.api.AfterAll;
+	import org.junit.jupiter.api.BeforeAll;
+	import org.junit.jupiter.api.Order;
+	import org.junit.jupiter.api.Test;
+
+	import businessLogic.PageViewer;
+	import domainModel.Channel;
+	import domainModel.ChannelsPanel;
+	import domainModel.RecommendedChannels;
+	import domainModel.Streamer;
+	import domainModel.Topic;
+        
+	class PageControllerStreamerTest {
+		private static PageControllerStreamer pcs;
+		private ChannelsPanel cp;
+		private Channel c;
+		private Topic t;
+		
+		@BeforeAll
+		static void setupBeforeClass( ) {
+			ChannelsPanel cp = new ChannelsPanel();
+			pcs = new PageControllerStreamer ("streamer di test", cp, "canale di test");
+			Channel c = cp.search("canale di test");
+			Topic t = new Topic("Gaming");
+		}
+		
+
+		@Test
+		void startLiveTest() { 
+			//Exercise
+			pcs.startLive("live");
+			//Verify
+		    assertEquals(true, c.getLive().isOnAir());
+		    assertEquals("live", c.getLive().getName());
+		    
+		    
+		}
+		
+		@Test
+		void stopLiveTest() {
+			//Exercise
+			pcs.startLive("live");
+			pcs.stopLive();
+			//Verify
+			assertEquals(false, c.getLive().isOnAir());
+			assertEquals(null, c.getLive().getName());
+		}
+		
+		@Test
+		void addTopicTest() {
+		    //Exercise
+			pcs.setTopic(t);
+			//Verify
+			assertEquals("Gaming", c.getTopic());
+		}
+		
+	}
+		
+
